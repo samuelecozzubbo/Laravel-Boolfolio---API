d1f4d0a98f184f316f182ec790138828f6a7bf67
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-09-30 16:17:56 +0200

    Get all project with technologies and type API

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -5,12 +5,20 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Project;
 use Illuminate\Http\Request;
+use App\Models\Technology;
+use App\Models\Type;
+
+
 
 class PageController extends Controller
 {
     public function index()
     {
-        $project = Project::all();
-        return response()->json($project);
+        $project = Project::orderBy('id')->with('technologies', 'type')->paginate(10);
+        $result = [
+            'success' => true,
+            'results' => $project,
+        ];
+        return response()->json($result);
     }
 }
