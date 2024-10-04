b82c86a6a13c65a48cf92eaffc904e36a2aff40e
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-10-04 16:35:02 +0200

    Lead controller validation

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
@@ -11,6 +12,33 @@ class LeadController extends Controller
     {
         $data = $request->all();
         $success = true;
+
+        //VALIDAZIONE
+        $validator = Validator::make(
+            $data,
+            [
+                'name' => 'required|string|max:100',
+                'email' => 'required|email',
+                'message' => 'required|string|min:10',
+            ],
+            [
+                'name.required' => 'Il campo nome è obbligatorio.',
+                'name.string' => 'Il nome deve essere una stringa.',
+                'name.max' => 'Il nome non può superare i :max caratteri.',
+                'email.required' => 'Il campo email è obbligatorio.',
+                'email.email' => 'Inserisci un indirizzo email valido.',
+                'message.required' => 'Il campo messaggio è obbligatorio.',
+                'message.string' => 'Il messaggio deve essere una stringa.',
+                'message.min' => 'Il messaggio deve contenere almeno 10 caratteri.',
+            ]
+        );
+
+        if ($validator->fails()) {
+            $success = false;
+            $errors = $validator->errors();
+            return response()->json(compact('success', 'errors'));
+        }
+
         return response()->json(compact('success'));
     }
 }
